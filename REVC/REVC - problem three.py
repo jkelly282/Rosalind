670fd935f42b02b9732bde163df8ab6c3dd48a25
jkelly2 <jkellyis@yahoo.co.uk>
2021-05-10 21:35:56 +0100

    Third Problem

---
+++ REVC/REVC - problem three.py
@@ -0,0 +1,17 @@
+with open("rosalind_revc.txt") as f:
+    dna = f.read().strip()
+reverse_comp = []
+for i in dna:
+    if i == "A":
+        reverse_comp.append("T")
+    elif i == "T":
+        reverse_comp.append("A")
+    elif i == "G":
+        reverse_comp.append("C")
+    elif i == "C":
+        reverse_comp.append("G")
+    else:
+        print("Unknown character")
+
+reverse_comp.reverse()
+print(''.join(reverse_comp))
