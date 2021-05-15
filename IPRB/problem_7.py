585abd861c7b34ec24d890e03b0b093c6f78987c
jkelly2 <jkellyis@yahoo.co.uk>
2021-05-15 15:31:41 +0100

    Problem 7

---
+++ IPRB/problem_7.py
@@ -0,0 +1,13 @@
+dominant = 15
+hetero = 21
+recessive = 25
+
+total = dominant + hetero + recessive
+
+r_r = (recessive / total) * ((recessive - 1) / (total - 1))
+h_h = (hetero / total) * ((hetero - 1) / (total - 1))
+h_r = (hetero / total) * (recessive / (total - 1)) + (recessive / total) * (hetero / (total - 1))
+
+recessive_total = r_r + h_h * 1 / 4 + h_r * 1 / 2
+total = 1 - recessive_total
+print(total)
